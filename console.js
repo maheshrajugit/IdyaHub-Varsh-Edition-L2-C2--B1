9f6eb99138a66a1bf399a875957d4fe6e3da08e2
pavan <pavan@wibesapp.com>
2023-07-06 20:04:30 +0530

    Class 1

---
+++ console.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+console.log("Hello","World");
+console.log("Hello "+"World");
+
+a=6;
+b=5;
+console.log("a+b")
+console.log(a+b)
+console.log(a,b,a+b)
+console.log(a,"+",b,"=",a+b)
+console.log(a+" + "+b+" = "+(a+b))
+//console.log(a+" + "+b+" = "+parseInt(a+b))
+
---
+++ js.js
@@ -0,0 +1,4 @@
+a=5;
+b=54;
+
+console.log(b/a)
