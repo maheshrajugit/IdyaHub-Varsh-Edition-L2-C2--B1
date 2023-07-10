8ea3a2f49b2eb0412b1e489fba3721f607eb74c0
pavan <pavan@wibesapp.com>
2023-07-10 20:17:24 +0530

    assignment done

--- console.js
+++ console.js
@@ -13,8 +13,11 @@
 // console.log("End of lesson")
 a=2;
 b=6;
-console.log("the sum of", a, " and", b,"is", a+b)
-console.log("The difference of", a ,"and", b ,"is", a-b )
+console.log("the sum of", a, " and", b,"is", a+b);
+console.log("The difference of", a ,"and", b ,"is", a-b );
+console.log("The product of", a ,"and", b ,"is", a*b );
+console.log("The quotient of", a ,"and", b ,"is", a/b );
+console.log("The remainder of", a ,"and", b ,"is", a%b );
 
 
 
